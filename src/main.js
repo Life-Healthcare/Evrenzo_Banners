6c2057e72e7c35da8d0d0069f63263748e7d51d9
Moses <mosesaizee@outlook.com>
2022-01-04 16:10:10 +0000

    feat: added google create counter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ hrefs.forEach((href) => {
   href.addEventListener(
     "click",
     (event) => {
+      Enabler.counter(window.bType, true);
       event.stopPropagation();
       window.open(href.dataset.href, "_blank");
       Enabler.exit(href.dataset.href);
